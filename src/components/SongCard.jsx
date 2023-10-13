7918bb7d49cc211702bef9484eba01c0e595b2c3
Chris Kulwik <ckulwik@umich.edu>
2023-10-13 14:26:16 -0400

    init next app

--- src/components/SongCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/songCard.css";
-
-const SongCard = ({ hoverSpeed, children }) => {
-  return <div className={`card-container speed-${hoverSpeed}`}>{children}</div>;
-};
-
-export default SongCard;
